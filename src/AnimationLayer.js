94d5b9d46ecab4371072354528f88ccd40286f49
NavCat <yima1006@qq.com>
2015-07-22 16:44:56 +0800

    添加Chipmunk物理引擎

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -5,9 +5,11 @@
 var AnimationLayer = cc.Layer.extend({
 	spriteSheet: null,
 	sprite: null,
+	space: null,
 	runingAction: null,
-	ctor: function(){
+	ctor: function(space){
 		this._super();
+		this.space = space;
 		this.init();
 	},
 	/**
@@ -15,6 +17,7 @@ var AnimationLayer = cc.Layer.extend({
 	 */
 	init: function(){
 		this._super();
+		
 		// 加载精灵表到cash
 		cc.spriteFrameCache.addSpriteFrames(res.Running_plist);
 		this.spriteSheet = new cc.SpriteBatchNode(res.Runner_png);
@@ -34,6 +37,28 @@ var AnimationLayer = cc.Layer.extend({
 		// 用一个持续重复动作封装该动画
 		this.runingAction = new cc.RepeatForever(new cc.Animate(animation));
 		
+		//1. create PhysicsSprite with a sprite frame name
+		this.sprite = new cc.PhysicsSprite("#runner0.png");
+		var contentSize = this.sprite.getContentSize();
+		// 2. init the runner physic body
+		this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
+		//3. set the position of the runner
+		this.body.p = cc.p(g_runnerStartX, g_groundHeight + contentSize.height / 2);
+		//4. apply impulse to the body
+		this.body.applyImpulse(cp.v(150, 0), cp.v(0, 0));//run speed
+		//5. add the created body to space
+		this.space.addBody(this.body);
+		//6. create the shape for the body
+		this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
+		//7. add shape to space
+		this.space.addShape(this.shape);
+		//8. set body to the physic sprite
+		this.sprite.setBody(this.body);
+		
+		this.sprite.runAction(this.runingAction);
+		this.spriteSheet.addChild(this.sprite);
+
+		/**
 		this.sprite = new cc.Sprite("#runner0.png");
 		this.sprite.attr({
 			x: 80,
@@ -41,15 +66,17 @@ var AnimationLayer = cc.Layer.extend({
 		});
 		this.sprite.runAction(this.runingAction);
 		this.spriteSheet.addChild(this.sprite);
+		*/
 		
-		
-		//var spritRuner = new cc.Sprite(res.Runner_png);
-		//spritRuner.attr({
-		//	x: 80,
-		//	y: 85
-		//});
-		//var actionTo = new cc.moveTo(2, cc.p(300, 85));
-		//spritRuner.runAction(new cc.Sequence(actionTo));
-		//this.addChild(spritRuner);
+		/**
+		var spritRuner = new cc.Sprite(res.Runner_png);
+		spritRuner.attr({
+			x: 80,
+			y: 85
+		});
+		var actionTo = new cc.moveTo(2, cc.p(300, 85));
+		spritRuner.runAction(new cc.Sequence(actionTo));
+		this.addChild(spritRuner);
+		**/
 	}
 });
--- src/PlayScene.js
+++ src/PlayScene.js
@@ -2,11 +2,33 @@
  * 跑步场景
  */
 var PlayScene = cc.Scene.extend({
+	space:null,   // 物理边界
 	onEnter: function(){
 		this._super();
 		
+		this.initPhysics();
+		
 		this.addChild(new RuningBgLayer());
-		this.addChild(new AnimationLayer());
+		this.addChild(new AnimationLayer(this.space));
 		this.addChild(new StatusLayer());
+		
+		this.scheduleUpdate();
+	},
+	initPhysics: function(){
+		//1. new space object 
+		this.space = new cp.Space();
+		//2. setup the  Gravity
+		this.space.gravity = cp.v(0, -350);
+
+		// 3. set up Walls
+		var wallBottom = new cp.SegmentShape(this.space.staticBody,
+				cp.v(0, g_groundHeight),// start point
+				cp.v(4294967295, g_groundHeight),// MAX INT:4294967295
+				0);// thickness of wall
+		this.space.addStaticShape(wallBottom);
+	},
+	update:function (dt) {
+		// chipmunk step
+		this.space.step(dt);
 	}
 });
