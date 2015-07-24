fc8eba6ef0c616d5c2a0adaa8f5071ca46ba95bd
NavCat <yima1006@qq.com>
2015-07-24 21:12:20 +0800

    按场景分类

--- src/resource.js
+++ src/resource.js
@@ -1,17 +1,17 @@
 var res = {
-	HelloBG_png : "res/helloBG.png",    // 开始界面的背景
-	Start_n_png : "res/start_n.png",    // 开始按钮
-	Start_s_png : "res/start_s.png",
-	PlayBG_png  : "res/PlayBG.png",     // 跑步背景
-	Runner_png  : "res/running.png",		// 奔跑者
-	Running_plist : "res/running.plist", 	// 奔跑者
-	map_png: "res/map.png",					// TiledMAP
-	map00_tmx: "res/map00.tmx",
-	map01_tmx: "res/map01.tmx",
-	background_png :"res/background.png",    // 金币及岩石
-	background_plist : "res/background.plist",
-	restart_n_png : "res/restart_n.png",
-	restart_s_png : "res/restart_s.png"
+		HelloBG_png : "res/gamePlay/helloBG.png",    // 开始界面的背景
+		Start_n_png : "res/gamePlay/start_n.png",    // 开始按钮
+		Start_s_png : "res/gamePlay/start_s.png",
+		PlayBG_png  : "res/gamePlay/PlayBG.png",     // 跑步背景
+		Runner_png  : "res/gamePlay/running.png",		// 奔跑者
+		Running_plist : "res/gamePlay/running.plist", 	// 奔跑者
+		map_png: "res/gamePlay/map.png",					// TiledMAP
+		map00_tmx: "res/gamePlay/map00.tmx",
+		map01_tmx: "res/gamePlay/map01.tmx",
+		background_png :"res/gamePlay/background.png",    // 金币及岩石
+		background_plist : "res/gamePlay/background.plist",
+		restart_n_png : "res/gameOver/restart_n.png",
+		restart_s_png : "res/gameOver/restart_s.png"
 };
 
 var g_resources = [];
